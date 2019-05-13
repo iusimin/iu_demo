48474b31fa83964c90240cfea60dccf93e25b5a2
iusimin <linsimin@h1n1.onaliyun.com>
2019-05-13 16:19:08 +0800

    support dbshell

--- wms/model/mongo/__init__.py
+++ wms/model/mongo/__init__.py
@@ -1,1 +1,12 @@
 IU_DEMO_DB = 'iu'
+
+from wms.model.mongo.combine_parcel.combine_pool import *
+from wms.model.mongo.combine_parcel.combined_logistics_order import *
+from wms.model.mongo.combine_parcel.inbound_parcel import *
+from wms.model.mongo.combine_parcel.operation_record import *
+from wms.model.mongo.combine_parcel.outbound_parcel import *
+from wms.model.mongo.combine_parcel.sort_job import *
+
+from wms.model.mongo.logistics.logistics_carrier import *
+from wms.model.mongo.logistics.logistics_order import *
+from wms.model.mongo.logistics.logistics_platform import *
